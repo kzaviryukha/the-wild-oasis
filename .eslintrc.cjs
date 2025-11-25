184e5f86b63305e5797bf68ef98e21bf9e94a4d8
m9iv <aethawalka@gmail.com>
2025-11-25 12:23:41 +0200

    Add fetching cabins from supabase with React Query

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,8 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    "no-unused-vars": "off",
+    "no-unused-vars": "warn",
+    "react/prop-types": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
