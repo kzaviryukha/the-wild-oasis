cd2906934f6910876e1c1f0854a2887352c86506
m9iv <aethawalka@gmail.com>
2026-01-02 11:45:57 +0200

    Add Portal for Modal

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from "react"
-import { getCabins } from "../services/apiCabins"
 import Heading from "../ui/Heading"
 import Row from "../ui/Row"
 import CabinTable from "../features/cabins/CabinTable"
-import Button from "../ui/Button"
-import CreateCabinForm from "../features/cabins/CreateCabinForm"
+
+import AddCabin from "../features/cabins/AddCabin"
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false)
-
   return (
     <>
       <Row type="horizontal">
@@ -19,10 +15,7 @@ function Cabins() {
       <Row>
         <CabinTable />
 
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Create new cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   )
