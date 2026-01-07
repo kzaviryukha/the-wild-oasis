d84c57e8f282adfac72a3ea4f5bfcaf384b108b1
m9iv <aethawalka@gmail.com>
2026-01-07 13:02:30 +0200

    Add clientside filtering Cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,13 +3,15 @@ import Row from "../ui/Row"
 import CabinTable from "../features/cabins/CabinTable"
 
 import AddCabin from "../features/cabins/AddCabin"
+import CabinTableOperations from "../features/cabins/CabinTableOperations"
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+
+        <CabinTableOperations />
       </Row>
 
       <Row>
