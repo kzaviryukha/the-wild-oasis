b00bdc15d9a3075dc72e1dffbd86643411771190
m9iv <aethawalka@gmail.com>
2026-01-27 11:19:31 +0200

    Add Bookings Table

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,18 +1,18 @@
-import styled from "styled-components";
-import { format, isToday } from "date-fns";
+import styled from "styled-components"
+import { format, isToday } from "date-fns"
 
-import Tag from "../../ui/Tag";
-import Table from "../../ui/Table";
+import Tag from "../../ui/Tag"
+import Table from "../../ui/Table"
 
-import { formatCurrency } from "../../utils/helpers";
-import { formatDistanceFromNow } from "../../utils/helpers";
+import { formatCurrency } from "../../utils/helpers"
+import { formatDistanceFromNow } from "../../utils/helpers"
 
 const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
   font-family: "Sono";
-`;
+`
 
 const Stacked = styled.div`
   display: flex;
@@ -27,12 +27,12 @@ const Stacked = styled.div`
     color: var(--color-grey-500);
     font-size: 1.2rem;
   }
-`;
+`
 
 const Amount = styled.div`
   font-family: "Sono";
   font-weight: 500;
-`;
+`
 
 function BookingRow({
   booking: {
@@ -52,7 +52,7 @@ function BookingRow({
     unconfirmed: "blue",
     "checked-in": "green",
     "checked-out": "silver",
-  };
+  }
 
   return (
     <Table.Row>
@@ -80,7 +80,7 @@ function BookingRow({
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
     </Table.Row>
-  );
+  )
 }
 
-export default BookingRow;
+export default BookingRow
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,15 +1,18 @@
+import { useSearchParams } from "react-router-dom"
 import { useCabins } from "./useCabins"
 import Spinner from "../../ui/Spinner"
 import CabinRow from "./CabinRow"
 import Table from "../../ui/Table"
 import Menus from "../../ui/Menus"
-import { useSearchParams } from "react-router-dom"
+import Empty from "../../ui/Empty"
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins()
   const [searchParams] = useSearchParams()
 
   if (isLoading) return <Spinner />
+
+  if (!cabins.length) return <Empty resourceName="cabins" />
 
   // 1. Filter
   const filterValue = searchParams.get("discount") || "all"
@@ -26,7 +29,7 @@ function CabinTable() {
   const [field, direction] = sortBy.split("-")
   const modifier = direction === "asc" ? 1 : -1
   const sortedCabins = filteredCabins.sort(
-    (a, b) => (a[field] - b[field]) * modifier
+    (a, b) => (a[field] - b[field]) * modifier,
   )
 
   return (
