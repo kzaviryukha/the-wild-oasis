6ac7a7a960d0146209300d15a827486c2f183f89
m9iv <aethawalka@gmail.com>
2026-02-10 14:58:07 +0200

    Add API-side pagination for Bookings table

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,11 +1,14 @@
 import { getToday } from "../utils/helpers"
+import { PAGE_SIZE } from "../utils/constants"
 import supabase from "./supabase"
 
-// id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
-    .select("*, cabins(name), guests(fullName, email)")
+    .select(
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
+      { count: "exact" },
+    )
 
   // Filter
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value)
@@ -14,14 +17,21 @@ export async function getBookings({ filter, sortBy }) {
   if (sortBy)
     query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" })
 
-  const { data, error } = await query
+  if (page) {
+    const from = (page - 1) * (PAGE_SIZE - 1)
+    const to = from + PAGE_SIZE - 1
+
+    query = query.range(from, to)
+  }
+
+  const { data, error, count } = await query
 
   if (error) {
     console.error(error)
     throw new Error("Bookings could not be loaded")
   }
 
-  return data
+  return { data, count }
 }
 
 export async function getBooking(id) {
